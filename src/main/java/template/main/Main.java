ed8615ff72240168c9235c709ff0722d51aa2dbe
GawlikK <GawlikK@ACS14.ecs.baylor.edu>
2014-10-05 13:13:06 -0500

    Why do I need a message?

--- src/main/java/template/main/Main.java
+++ src/main/java/template/main/Main.java
@@ -3,6 +3,7 @@ package template.main;
 public class Main {
 	public static void main(String[] args) {
 		System.setProperty("environment", "dev");
+		System.out.println("Hi, my name is not Bob. What name is your's not?");
 		Application.run(args);
 	}
 }
