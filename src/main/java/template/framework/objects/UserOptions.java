1fa827f5547dfaf96d3d45792c97fd4179bcbe8f
Philip-Spencer1 <Philip_Spencer1@Baylor.edu>
2014-10-05 13:45:43 -0500

    Added object class for user input

---
+++ src/main/java/template/framework/objects/UserOptions.java
@@ -0,0 +1,14 @@
+package template.framework.objects;
+
+public class UserOptions {
+	 
+	    private String locationName;
+	    private int income;
+	    private int relationshipStatus;
+	    private int age;
+	    private int communityType;
+	    private int schoolImportance;
+	    
+	 
+	    // getters and setters...
+}
