1ff0677d665ded70185657f0acedb98a2f874806
Philip-Spencer1 <Philip_Spencer1@Baylor.edu>
2014-10-05 14:14:43 -0500

    Temp Submit

--- src/main/java/template/controllers/MainController.java
+++ src/main/java/template/controllers/MainController.java
@@ -3,18 +3,35 @@ package template.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import template.framework.objects.UserOptions;
 
 @Controller
 public class MainController {
-	@RequestMapping("/")
-	public String home() {
-
+	@RequestMapping(value="/", method=RequestMethod.GET)
+	public String home(Model model) 
+	{
+		model.addAttribute("index", new UserOptions());
 		return "index";
 	}
 	
-	@RequestMapping("/map")
-	public String map() {
+/*	@RequestMapping(value="/map", method=RequestMethod.POST)
+	public String formSubmit() 
+	{
 
+		return "map";
+	}
+	
+*/	
+	@RequestMapping(value="/", method=RequestMethod.POST)
+	public String formSubmit(@ModelAttribute UserOptions userOptions, Model model)
+	{
+		model.addAttribute("index", userOptions);
 		return "map";
 	}
 }
