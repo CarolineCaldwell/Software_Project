bfbc1666a9f215ebbe38b175ab9c009fcfd3bc72
GawlikK <GawlikK@ACS14.ecs.baylor.edu>
2014-10-05 13:35:35 -0500

    Testing commit ability

--- src/main/java/template/main/Main.java
+++ src/main/java/template/main/Main.java
@@ -4,5 +4,6 @@ public class Main {
 	public static void main(String[] args) {
 		System.setProperty("environment", "dev");
 		Application.run(args);
+//////////////////////////////////////////////////////////////
 	}
 }
