5854209b62506b39d27e120981c2bd56d8e1df6a
GawlikK <GawlikK@ACS14.ecs.baylor.edu>
2014-10-05 13:14:50 -0500

    Message

--- src/main/java/template/main/Main.java
+++ src/main/java/template/main/Main.java
@@ -3,7 +3,12 @@ package template.main;
 public class Main {
 	public static void main(String[] args) {
 		System.setProperty("environment", "dev");
-		System.out.println("Hi, my name is not Bob. What name is your's not?");
+		System.out.println("Test1");
+		System.out.println("Test2");
+		System.out.println("Test3");
+		System.out.println("Test4");
+		System.out.println("Test5");
+		System.out.println("Test6");
 		Application.run(args);
 	}
 }
