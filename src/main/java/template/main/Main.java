bd9905765e11d545f46414e375e85be8951808e4
GawlikK <GawlikK@ACS14.ecs.baylor.edu>
2014-10-05 13:34:17 -0500

    Fixed?

--- src/main/java/template/main/Main.java
+++ src/main/java/template/main/Main.java
@@ -3,13 +3,6 @@ package template.main;
 public class Main {
 	public static void main(String[] args) {
 		System.setProperty("environment", "dev");
-		System.out.println("Test1");
-		System.out.println("Test2");
-		System.out.println("Test3");
-		System.out.println("Test4");
-		System.out.println("Test5");
-		System.out.println("Test6");
-		System.out.println("Test7");
 		Application.run(args);
 	}
 }
