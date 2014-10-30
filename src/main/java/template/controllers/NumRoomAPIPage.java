fddc5fb4339b41e7e0ae6be01e3b09e09bf3ce89
caldwell-reed <caldwell-reed@SDL26.ecs.baylor.edu>
2014-10-29 23:59:11 -0500

    Adding new files

---
+++ src/main/java/template/controllers/NumRoomAPIPage.java
@@ -0,0 +1,11 @@
+package template.controllers;
+
+import template.framework.objects.UserOptions;
+
+public class NumRoomAPIPage {
+	
+	public void callNumRoom(Info info, UserOptions options) {
+		
+	}
+
+}
