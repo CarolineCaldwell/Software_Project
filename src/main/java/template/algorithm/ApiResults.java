0ccbdbd20ab88d88b68ee8f279d965273867f6bc
caldwell-reed <caldwell-reed@SDL26.ecs.baylor.edu>
2014-10-30 00:02:07 -0500

    Added Corbin's files. There is an error with his branch and pushing.

---
+++ src/main/java/template/algorithm/ApiResults.java
@@ -0,0 +1,112 @@
+package template.algorithm;
+
+public class ApiResults 
+{
+	private int income;
+	private int incomeTotal;
+	private int incomeWeight;
+	private boolean incomeIncluded;
+	private int relation;
+	private int relationTotal;
+	private int relationWeight;
+	private boolean relationIncluded;
+	private int age;
+	private int ageTotal;
+	private int ageWeight;
+	private boolean ageIncluded;
+	private int[] school;
+	private int schoolWeight;
+	private boolean schoolIncluded;
+	
+	public int getIncome() {
+		return income;
+	}
+	public void setIncome(int income) {
+		this.income = income;
+	}
+	public int getIncomeTotal() {
+		return incomeTotal;
+	}
+	public void setIncomeTotal(int incomeTotal) {
+		this.incomeTotal = incomeTotal;
+	}
+	public int getIncomeWeight() {
+		return incomeWeight;
+	}
+	public void setIncomeWeight(int incomeWeight) {
+		this.incomeWeight = incomeWeight;
+	}
+	public boolean isIncomeIncluded() {
+		return incomeIncluded;
+	}
+	public void setIncomeIncluded(boolean incomeIncluded) {
+		this.incomeIncluded = incomeIncluded;
+	}
+	public int getRelation() {
+		return relation;
+	}
+	public void setRelation(int relation) {
+		this.relation = relation;
+	}
+	public int getRelationTotal() {
+		return relationTotal;
+	}
+	public void setRelationTotal(int relationTotal) {
+		this.relationTotal = relationTotal;
+	}
+	public int getRelationWeight() {
+		return relationWeight;
+	}
+	public void setRelationWeight(int relationWeight) {
+		this.relationWeight = relationWeight;
+	}
+	public boolean isRelationIncluded() {
+		return relationIncluded;
+	}
+	public void setRelationIncluded(boolean relationIncluded) {
+		this.relationIncluded = relationIncluded;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public int getAgeTotal() {
+		return ageTotal;
+	}
+	public void setAgeTotal(int ageTotal) {
+		this.ageTotal = ageTotal;
+	}
+	public int getAgeWeight() {
+		return ageWeight;
+	}
+	public void setAgeWeight(int ageWeight) {
+		this.ageWeight = ageWeight;
+	}
+	public boolean isAgeIncluded() {
+		return ageIncluded;
+	}
+	public void setAgeIncluded(boolean ageIncluded) {
+		this.ageIncluded = ageIncluded;
+	}
+	public int[] getSchool() {
+		return school;
+	}
+	public void setSchool(int[] school) {
+		this.school = school;
+	}
+	public int getSchoolWeight() {
+		return schoolWeight;
+	}
+	public void setSchoolWeight(int schoolWeight) {
+		this.schoolWeight = schoolWeight;
+	}
+	public boolean isSchoolIncluded() {
+		return schoolIncluded;
+	}
+	public void setSchoolIncluded(boolean schoolIncluded) {
+		this.schoolIncluded = schoolIncluded;
+	}
+	
+}
